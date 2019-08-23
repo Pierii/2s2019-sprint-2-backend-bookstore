80e98de50e578de3ef9297d1d12a53a3ed67b5c6
Rafael Pieri Almeida <46181942866@info.local>
2019-08-23 11:24:56 -0300

    23.08.2019T11:24

---
+++ Senai.BookStore.WebApi/Senai.BookStore.WebApi/Controllers/AutoresController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai.BookStore.WebApi.Domains;
+using Senai.BookStore.WebApi.Repositories;
+
+namespace Senai.BookStore.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class AutoresController : ControllerBase
+    {
+        AutorRepository AutorRepository = new AutorRepository();
+
+        [HttpGet]
+        public IEnumerable<AutorDomain> Listar()
+        {
+            return AutorRepository.Listar();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(AutorDomain autorDomain)
+        {
+            AutorRepository.Cadastrar(autorDomain);
+            return Ok();
+        }
+    }
+}
---
+++ Senai.BookStore.WebApi/Senai.BookStore.WebApi/Repositories/AutorRepository.cs
@@ -0,0 +1,63 @@
+﻿using Senai.BookStore.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.BookStore.WebApi.Repositories
+{
+    public class AutorRepository
+    {
+        private string StringConexao = "Data Source=.\\SqlExpress; Initial catalog=M_BookStore; User Id=sa; Pwd=132";
+
+        public List<AutorDomain> Listar()
+        {
+
+        List<AutorDomain> autores = new List<AutorDomain>();
+            using (SqlConnection con = new SqlConnection(StringConexao))
+            {
+                string Query = "SELECT * FROM Autores ORDER BY IdAutor ASC";
+
+                con.Open();
+
+                SqlDataReader rdr;
+
+                using (SqlCommand cmd = new SqlCommand(Query, con))
+
+                    rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    AutorDomain autor = new AutorDomain
+                    {
+                        IdAutor = Convert.ToInt32(rdr["IdAutor"]),
+                        Nome = rdr["Nome"].ToString(),
+                        Email = rdr["Email"].ToString(),
+                        Ativo = Convert.ToByte(rdr["Ativo"]),
+                        DataNascimento = Convert.ToDateTime(rdr["DataNascimento"])
+                    };
+                    autores.Add(autor);
+                }
+            }
+            return autores;
+        }
+
+        public void Cadastrar(AutorDomain autor)
+        {
+            string Query = "INSERT INTO Autores (Nome, Email, Ativo, DataNascimento) VALUES (@Nome, @Email, @Ativo, @DataNascimento)";
+
+            using (SqlConnection con = new SqlConnection(StringConexao))
+            {
+                SqlCommand cmd = new SqlCommand(Query, con);
+                cmd.Parameters.AddWithValue("@Nome", autor.Nome);
+                cmd.Parameters.AddWithValue("@Email", autor.Email);
+                cmd.Parameters.AddWithValue("@Ativo", autor.Ativo);
+                cmd.Parameters.AddWithValue("@DataNascimento", autor.DataNascimento);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
